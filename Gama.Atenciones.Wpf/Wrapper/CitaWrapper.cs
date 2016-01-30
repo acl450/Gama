8692785fc3306f8d3d252bf2a85619e802ec09bb
Alberto Cardona López <alberto.cardona450@gmail.com>
2016-01-30 06:03:22 +0000

    Integrada AtencionWrapper en CitaWrapper

--- Gama.Atenciones.Wpf/Wrapper/CitaWrapper.cs
+++ Gama.Atenciones.Wpf/Wrapper/CitaWrapper.cs
@@ -9,8 +9,19 @@ namespace Gama.Atenciones.Wpf.Wrapper
 {
     public class CitaWrapper : ModelWrapper<Cita>
     {
-        public CitaWrapper(Cita modelo) : base(modelo)
+        public CitaWrapper(Cita model) : base(model)
         {
+            InitializeComplexProperties(model);
+        }
+
+        private void InitializeComplexProperties(Cita model)
+        {
+            if (model.Atencion == null)
+            {
+                throw new ArgumentException("La atención no puede ser nula");
+            }
+
+            this.Atencion = new AtencionWrapper(model.Atencion);
         }
 
         public DateTime Fecha
@@ -31,11 +42,12 @@ namespace Gama.Atenciones.Wpf.Wrapper
             set { SetValue(value); }
         }
 
-
         public string Sala
         {
             get { return GetValue<string>(); }
             set { SetValue(value); }
         }
+
+        public AtencionWrapper Atencion { get; private set; }
     }
 }
